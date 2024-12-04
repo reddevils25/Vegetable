323397563baf292ad10e7e67569b422012950f43
Phương Hoàng <phuongzbz1101@gmail.com>
2024-12-04 16:59:41 +0700

    thêm login

---
+++ Vegetable/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vegetable.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string Username, string Password, bool RememberMe)
+        {
+            // Giả lập kiểm tra thông tin đăng nhập
+            if (Username == "admin" && Password == "12345")
+            {
+                // Đăng nhập thành công
+                HttpContext.Session.SetString("AdminUsername", Username);
+                return RedirectToAction("Index");
+            }
+
+            // Đăng nhập thất bại
+            ViewBag.ErrorMessage = "Invalid username or password.";
+            return View();
+        }
+    }
+}
