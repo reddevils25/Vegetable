c25f9beb80e5d682c7a5e527fd1c15459ec07b64
Phương Hoàng <phuongzbz1101@gmail.com>
2024-12-11 11:26:57 +0700

    ChectOut

---
+++ Vegetable/Controllers/ChectOutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vegetable.Controllers
+{
+    public class ChectOutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
